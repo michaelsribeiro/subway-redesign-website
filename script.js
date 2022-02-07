258480f6dfd7c5f2cb7eb5f48d6c580e7f6cf0a5
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-02-06 23:35:11 -0300

    feat: show modal, fill infos of product and functionality of add to cart button

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 AOS.init();
 
-// Carousel
+// => Animation Loop buyButton
+$(document).ready( () => {
+    let button = document.querySelector('.nav-bar a:last-child');
+    setInterval( () => {        
+        if (button.classList.contains('animation')) {
+            document.querySelector('.nav-bar a:last-child').classList.remove('animation');
+        } else {        
+            button.classList.add('animation');
+        }
+    }, 3500);
+
+});
+
+// => Scroll to section animation
+const buyButton = document.querySelector('.nav-bar a:last-child');
+
+buyButton.addEventListener('click', scrollOnClick);
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const to = getScrollTopByHref(event.target) - 80;
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
+
+// => Carousel
 let currentSlide = 0;
 let totalSlides = document.querySelectorAll('.carousel-item').length;
 document.querySelector('.carousel-inner').style.width = `calc(100vw * ${totalSlides})`;
@@ -22,14 +59,99 @@ function updateMargin() {
     let newMargin = (currentSlide * sliderWidth);
     document.querySelector('.carousel-inner').style.marginLeft = `-${newMargin}px`;
 }
-
 setInterval(goNext, 8000);
 
+// => Sandwiches section
+let cart = [];
+let quant = 1;
+let pizzaKey = 0;
+
 pizzaJson.map( (item, index) => {
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+    const pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.setAttribute('data-key', index);
 
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `${item.price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} (${item.sizes[0]})`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let key = event.target.closest('.pizza-item').getAttribute('data-key');
+        quant = 1;
+        pizzaKey = key;
+
+        document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
+        document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `${pizzaJson[key].price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`;
+        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+
+        document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 1) size.classList.add('selected');
+        });
+
+        document.querySelector('.pizzaInfo--qt').innerHTML = quant;
+
+        document.querySelector('.pizzaWindowArea').style.opacity = 0;
+        document.querySelector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {            
+            document.querySelector('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
+
     document.querySelector('.pizza-area').append( pizzaItem );
 });
+
+function closeModal() {
+    document.querySelector('.pizzaWindowArea').style.opacity = 0;
+    setTimeout( () => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none';
+    }, 200);
+}
+
+document.querySelectorAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach( (item) => {
+    item.addEventListener('click', closeModal);
+});
+
+document.querySelector('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    quant > 1 ? quant -= 1 : quant;
+    document.querySelector('.pizzaInfo--qt').innerHTML = quant;
+});
+
+document.querySelector('.pizzaInfo--qtmais').addEventListener('click', () => {
+    quant++;
+    document.querySelector('.pizzaInfo--qt').innerHTML = quant;
+});
+
+document.querySelectorAll('.pizzaInfo--size').forEach( (size) => {
+    size.addEventListener('click', () => {
+        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+
+document.querySelector('.pizzaInfo--addButton').addEventListener('click', () => {
+    let size = parseInt(document.querySelector('.pizzaInfo--size.selected').getAttribute('data-key'));
+    let identifier = pizzaJson[pizzaKey].id + '-' + size;
+    let key = cart.findIndex( (item) => item.identifier == identifier);
+
+    if ( key > -1 ) {
+        cart[key].qtCart += quant; // => Se já houver um item no carrinho com o mesmo identifier, add a quant.
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[pizzaKey].id,
+            size,
+            qtCart: quant,
+        });
+    }
+    closeModal();
+    updateCart();
+});
+
+function updateCart() {
+    if (cart.length > 0) {
+        document.querySelector('.cartsection').classList.add('show');
+    }
+}
